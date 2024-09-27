e00daed3245330c6fa1521770ad3ecd4c13d5475
nicolegherrera <nickiiherrera@gmail.com>
2024-09-27 18:27:24 +0200

    new line

--- doc/script.py
+++ doc/script.py
@@ -4,3 +4,7 @@
 def P_disease_given_A(P_disease, P_A_given_disease, P_A_given_no_disease):
     P_A = P_A_given_disease * P_disease + P_A_given_no_disease * (1 - P_disease)
     return (P_A_given_disease * P_disease) / P_A
+
+def P_disease_given_A(P_disease, P_A_given_disease, P_A_given_no_disease):
+    P_A = P_A_given_disease * P_disease + P_A_given_no_disease * (1 - P_disease)
+    return (P_A_given_disease * P_disease) / P_A
